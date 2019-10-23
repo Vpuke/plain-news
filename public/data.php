654add64fe9d0f050524282934d8a15706f439f1
Viktor <viktorpuke@gmail.com>
2019-10-23 10:52:02 +0200

    Added arrays

---
+++ public/data.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+// Added arrays for 10 news feed posts.
+
+$newsPosts = [
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+    [
+        'title' => '',
+        'author' => '',
+        'content' => '',
+        'publishDate' => '',
+        'likeCounter' => '',
+        'contentIMG' => '',
+        'articleID' => ''
+    ],
+];
+
+// added 5 authors in an array with firstname, lastname and photo
+
+$authors = [
+    [
+        'firstname' => 'Viktor',
+        'lastname' => 'Puke',
+        'imgURL' => ''
+    ],
+    [
+        'firstname' => 'Erik',
+        'lastname' => 'Johannesson',
+        'imgURL' => ''
+    ],
+    [
+        'firstname' => 'Betsy',
+        'lastname' => 'Soplin',
+        'imgURL' => ''
+    ],
+    [
+        'firstname' => 'Bernhard',
+        'lastname' => 'Stedt',
+        'imgURL' => ''
+    ],
+    [
+        'firstname' => 'Daniel',
+        'lastname' => 'Thorsen',
+        'imgURL' => ''
+    ],
+
+]
